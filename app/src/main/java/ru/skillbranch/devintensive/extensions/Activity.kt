d3763f9a959b8eda9712af4a04a12e320c23d65d
Aleksander <Lega1Puma@users.noreply.github.com>
2020-07-29 18:58:34 +0300

    hometask_3 first build

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,31 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+fun Activity.hideKeyboard() {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(window.decorView.windowToken, 0)
+}
+
+fun Activity.isKeyboardOpen():Boolean {
+    val r = Rect()
+    val rootView = et_message.rootView
+    rootView.getWindowVisibleDisplayFrame(r)
+    val height = rootView.measuredHeight
+    val diff = height - r.bottom - r.top * 2
+    return diff > 200
+}
+
+fun Activity.isKeyboardClosed():Boolean {
+    val r = Rect()
+    val rootView = et_message.rootView
+    rootView.getWindowVisibleDisplayFrame(r)
+    val height = rootView.measuredHeight
+    val diff = height - r.bottom - r.top * 2
+    return diff < 200
+}
+
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,115 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(
+    var status: Status = Status.NORMAL,
+    var question: Question = Question.NAME
+    ) {
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)) ,
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+    }
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion():Question
+    }
+
+    fun askQuestion(): String = when(question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+//        return if (question.answers.contains(answer)) {
+//            question = question.nextQuestion()
+//            "Отлично - ты справился\n${question.question}" to status.color
+//        } else {
+//            status = status.nextStatus()
+//            "Это неправильный ответ\n${question.question}" to status.color
+//        }
+
+        val reg: Regex?
+        var des: String
+
+        when (question) {
+            Question.NAME -> {
+                reg = Regex("^([A-ZА-Я])[a-zа-я]+$")
+                des = "Имя должно начинаться с заглавной буквы"
+            }
+            Question.PROFESSION -> {
+                reg = Regex("^[ a-zа-я]+$")
+                des = "Профессия должна начинаться со строчной буквы"
+            }
+            Question.MATERIAL -> {
+                reg = Regex("^[a-zа-я]+$")
+                des = "Материал не должен содержать цифр"
+            }
+            Question.BDAY -> {
+                reg = Regex("^[0-9]+$")
+                des = "Год моего рождения должен содержать только цифры"
+            }
+            Question.SERIAL -> {
+                reg = Regex("^[0-9]{7}$")
+                des = "Серийный номер содержит только цифры, и их 7"
+            }
+            else -> {
+                reg = null
+                des = "На этом все, вопросов больше нет"
+            }
+        }
+
+        if (reg == null) {
+            return des to status.color
+        } else {
+            if (reg.matches(answer)) {
+                if (question.answers.contains(answer.toLowerCase())) {
+                    question = question.nextQuestion()
+                    des = "Отлично - ты справился"
+                } else {
+                    if (status == Status.CRITICAL) {
+                        status = Status.NORMAL
+                        question = Question.NAME
+                        des = "Это неправильный ответ. Давай все по новой"
+                    } else {
+                        status = status.nextStatus()
+                        des = "Это неправильный ответ"
+                    }
+                }
+            }
+        }
+        return "$des\n${question.question}" to status.color
+    }
+}
